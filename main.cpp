fcc53b69887dbc42587fb799d1ea26bd67a80521
Arserebryakova <arserebryakova@edu.hse.ru>
2023-07-16 07:14:28 +0300

    Добавлен граф с местами, теперь маршруты не повторяют одних точек дважды

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 
 // Place - содержит значимость места и то, сколько времени надо покуколдить в нём
@@ -39,7 +38,10 @@ public:
         std::vector<Excursion> allExcursions;
         dfsWithoutRemembering(startPlace, {}, allExcursions, maxTime);
         std::sort(allExcursions.rbegin(), allExcursions.rend(),
-                  [](const auto &lhs, const auto &rhs) { return lhs.significance < rhs.significance; });
+                  [](const auto &lhs, const auto &rhs) {
+                      return lhs.significance == rhs.significance ? lhs.duration > rhs.duration : lhs.significance <
+                                                                                                  rhs.significance;
+                  });
         return allExcursions;
     }
 
@@ -56,6 +58,8 @@ private:
     dfsWithoutRemembering(size_t placeID, Excursion excursion, std::vector<Excursion> &allExcursions,
                           int64_t maxTime, int64_t routeDuration = 0) {
         excursion.duration += routeDuration + places[placeID].timeToVisit;
+        if (std::find(excursion.placesIDs.begin(), excursion.placesIDs.end(), placeID) != excursion.placesIDs.end())
+            return;
         if (excursion.duration > maxTime)
             return;
         excursion.placesIDs.push_back(placeID);
@@ -73,21 +77,33 @@ private:
 int main() {
     GraphEngine graphEngine(
             {
-                    {1,  5},
-                    {2,  10},
-                    {3,  5},
-                    {4,  10},
-                    {10, 20}
+                    {5, 2},
+                    {3, 2},
+                    {6, 2},
+                    {6, 3},
+                    {3, 2},
+                    {6, 4},
+                    {6, 4},
+                    {4, 2},
             },
             {
-                    {0, 1, 10},
-                    {1, 2, 10},
-                    {2, 3, 10},
-                    {3, 4, 10},
-                    {0, 4, 25}
+                    {0, 1, 2},
+                    {0, 2, 2},
+                    {1, 2, 1},
+                    {2, 3, 2},
+                    {3, 4, 2},
+                    {3, 5, 3},
+                    {4, 5, 2},
+                    {5, 6, 5},
+                    {6, 7, 2},
+                    {0, 3, 4},
+                    {2, 5, 5},
+                    {3, 6, 8},
+                    {5, 7, 5}
             }
     );
-    auto allExcursions = graphEngine.getAllSatisfyingPaths(0, 60);
+    auto allExcursions = graphEngine.getAllSatisfyingPaths(6, 40);
+    int i = 0;
     for (const auto &[placesIDs, significance, duration]: allExcursions) {
         std::cout << "S:" << significance << " D:" << duration << " P: ";
         bool first = true;
@@ -99,6 +115,8 @@ int main() {
             std::cout << placeID;
         }
         std::cout << std::endl;
+        if (++i > 5)
+            break;
     }
     return 0;
 }
